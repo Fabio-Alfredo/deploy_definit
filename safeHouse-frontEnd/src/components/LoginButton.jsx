50e0119990b2ecd868d59b1c243a9e39d2ad1079
Fabio-Alfredo <00210121@uca.edu.sv>
2024-05-07 19:22:13 -0600

    add: new style login buton

--- safeHouse-frontEnd/src/components/LoginButton.jsx
+++ safeHouse-frontEnd/src/components/LoginButton.jsx
@@ -1,18 +1,34 @@
 import React, { useState } from 'react';
-import { GoogleLogin } from '@react-oauth/google';
+import { GoogleLogin, useGoogleLogin } from '@react-oauth/google';
 import { credentialResponse } from '../service/connecctionGoogle';
 import { gapi } from 'gapi-script';
-const LoginButton = () => {
+import { FcGoogle } from "react-icons/fc";
 
-    return (
-        <div className='mt-6'>
-            <GoogleLogin
+
+/**<GoogleLogin
                 width={300}
                 theme='filled_black'
                 text='Iniciar con Google'
                 onSuccess={credentialResponse}
-            />
-        </div>
+            /> */
+
+const LoginButton = () => {
+
+    const login = useGoogleLogin({
+        onSuccess: credentialResponse,
+        onError: (error) => console.log('Login Failed:', error)
+    });
+
+
+    return (
+        <div className='mt-6 '>
+            <button
+                className='bg-white border-solid border-2 w-[283px] h-[59.98px] rounded-full flex justify-center items-center'
+                onClick={() => login()}>
+                <FcGoogle className='mr-3 size-8' />
+                Continue with Google
+            </button>
+        </div >
     );
 };
 
--- safeHouse-frontEnd/src/service/connecctionGoogle.js
+++ safeHouse-frontEnd/src/service/connecctionGoogle.js
@@ -1,17 +1,31 @@
+import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
-export const credentialResponse = async(response) => {
+export const credentialResponse = async (response) => {
     try {
-        const {credential}= await response;
-        const res = jwtDecode(credential);
-        const {email, name, picture} = res;
+        const { access_token } = response;
+        axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${access_token}`, {
+            headers: {
+                Authorization: `Bearer ${access_token}`,
+                Accept: 'application/json'
+            }
+        })
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch((err) => console.log(err));
+
+        //console.log(access_token)
+        //const res = jwtDecode(access_token);
+        //console.log(res)
+        /*const {email, name, picture} = res;
         const info = {
             email,
             name,
             picture
         }
-        console.log(info);
-    }catch (error) {
+        console.log({info});*/
+    } catch (error) {
         console.log(error);
     }
 
