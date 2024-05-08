e67051dafb4cb3a3ee75a22426f599c056cdf8ae
Fabio-Alfredo <00210121@uca.edu.sv>
2024-05-07 23:35:57 -0600

    add: dimension

--- safeHouse-frontEnd/src/page/Auth.jsx
+++ safeHouse-frontEnd/src/page/Auth.jsx
@@ -9,12 +9,12 @@ const Auth = () => {
     return (
         <div className='flex flex-col h-screen bg-HomeImg bg-cover bg-center items-center justify-center'>
             <div className='flex-1 flex flex-col items-center justify-center'>
-                <div className='flex flex-col w-full h-40 justify-end items-center  lg:items-start lg:relative '>
-                    <img className='w-40 h-16 lg:w-[234.48px] lg:h-[115.83px] lg:absolute lg:right-0 ' src={logo} />
-                    <p className='text-white font-popins text-center text-[24px] font-extralight pt-10 lg:text-[45px]'>BIENVENIDO A</p>
+                <div className='flex flex-col w-full h-40 justify-end items-center  md:items-start md:relative '>
+                    <img className='w-40 h-16 md:w-[234.48px] md:h-[115.83px] md:absolute md:right-0 ' src={logo} />
+                    <p className='text-white font-popins text-center text-[24px] font-extralight pt-10 md:text-[45px]'>BIENVENIDO A</p>
                 </div>
 
-                <p className='text-white font-brygada-1918 font-semibold  text-center text-[48px] pt-6 lg:text-[110px]' >
+                <p className='text-white font-brygada-1918 font-semibold  text-center text-[48px] pt-6 md:text-[110px]' >
                     RESIDENCIAL <br className='xl:hidden' />
                     HLVS
                 </p>
