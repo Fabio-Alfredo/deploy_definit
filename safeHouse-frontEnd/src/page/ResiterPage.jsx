ce6d8d908a0920fb3eeabb877c4ed72a363550c7
Fabio-Alfredo <00210121@uca.edu.sv>
2024-05-10 22:37:58 -0600

    add: socket conecction api

--- safeHouse-frontEnd/src/page/ResiterPage.jsx
+++ safeHouse-frontEnd/src/page/ResiterPage.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navigation, Input, CatalogRole } from '../components/registerComponents';
 import Button from '../components/Button';
 import logo from '../assets/Logo.svg';
 import { useForm } from '../hooks/useForm';
 
+
 const ResiterPage = () => {
 
-    const {name, email, role, InputChange}= useForm({
-        name: '',
+    const { userName, email, role, InputChange } = useForm({
+        userName: '',
         email: '',
-        role:''
+        role: ''
     })
 
-    const handleSubmit = (e) =>{
+    const handleSubmit = (e) => {
         e.preventDefault();
+
         const formValues = {
-            name: name,
+            name: userName,
             email: email,
-            role:role
+            role: role
         }
         console.log(formValues);
     }
@@ -30,9 +32,9 @@ const ResiterPage = () => {
             <div onSubmit={handleSubmit} className='flex items-center justify-center w-full bg-color-primary px-6 h-[90vh]'>
                 <form className=' w-full p-8 shadow-2xl rounded-3xl bg-white xl:h-3/4 xl:p-14 lg:w-1/2' > {/* query */}
                     <Navigation title={"Registrar usuario"} />
-                    <Input name={"name"} label={"Nombre:"} type={"text"} inputValue={name} inputOnchange={InputChange} />
-                    <Input name={"email"} label={"Email:"} type={"email"} inputValue={email} inputOnchange={InputChange}/>
-                    <CatalogRole inputValue={role} inputOnchange={InputChange}/>
+                    <Input name={"userName"} label={"Nombre:"} type={"text"} inputValue={userName} inputOnchange={InputChange} />
+                    <Input name={"email"} label={"Email:"} type={"email"} inputValue={email} inputOnchange={InputChange} />
+                    <CatalogRole inputValue={role} inputOnchange={InputChange} />
                     <Button value={"Registrar"} type={"submit"} name={"RegisterButton"} />
                 </form>
             </div>
