52666c09c45c3028f63ec676ba84b4059ec1943f
Fabio-Alfredo <00210121@uca.edu.sv>
2024-05-11 21:44:29 -0600

    add: responsive desing

--- safeHouse-frontEnd/src/components/registerComponents/CatalogRole.jsx
+++ safeHouse-frontEnd/src/components/registerComponents/CatalogRole.jsx
@@ -5,7 +5,7 @@ export const CatalogRole = ({ inputValue,inputOnchange}) => {
         <>
             <div className='grid grid-cols-3 sm:mx-12 items-center m-4 sm:m-10'> {/* query */}
                 <label htmlFor='role' className='col-span-1 font-popins text-sm sm:text-2xl'> Role: </label>{/* query */}
-                <select name='role' required value={inputValue} onChange={inputOnchange} className='col-span-2 p-1 shadow-md  bg-input-color rounded-md text-xs sm:text-xl h-6 sm:h-10 w-5/12'> {/* query */}
+                <select name='role' required  value={inputValue} onChange={inputOnchange} className='col-span-2 p-1 ml-5  shadow-md font-popins  bg-input-color rounded-md text-xs xl:ml-0 sm:text-xl h-6 sm:h-10 w-5/12'> {/* query */}
                 <option value='' disabled>Role</option>
                     <option value='user'>User</option>
                     <option value='guest'>Guest</option>
--- safeHouse-frontEnd/src/components/registerComponents/Input.jsx
+++ safeHouse-frontEnd/src/components/registerComponents/Input.jsx
@@ -37,7 +37,7 @@ export const Input = ({ name, label, type, inputValue, inputOnchange }) => {
             <div className='grid grid-cols-3 sm:mx-12 items-center m-4 sm:m-10'>
                 <label htmlFor={name} className='col-span-1 font-popins text-sm sm:text-2xl'> {label} </label> {/* query */}
                 <input required type={type} name={name} value={inputValue} onChange={inputOnchange}
-                    className='col-span-2 p-1 shadow-md font-popins bg-input-color rounded-md  text-xs sm:text-xl h-6 sm:h-10 w-10/12' />
+                    className='col-span-2 p-1 ml-5 shadow-md font-popins bg-input-color rounded-md  text-xs xl:ml-0 sm:text-xl h-6 sm:h-10 w-10/12' />
                 {/*name === 'email' && !userExists && <p className="text-red-500 col-span-3 text-center pt-1">El correo electrónico no existe en la base de datos</p>*/}
             </div>
         </>
