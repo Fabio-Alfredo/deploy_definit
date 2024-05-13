6882ebbd37b0563c2c4a67eb153329e8e9aa0480
Fabio-Alfredo <00210121@uca.edu.sv>
2024-05-13 10:07:23 -0600

    add: new style qr

--- safeHouse-frontEnd/src/components/QrComponents/CreateQr.jsx
+++ safeHouse-frontEnd/src/components/QrComponents/CreateQr.jsx
@@ -22,76 +22,64 @@ const CreateQr = ({ info }) => {
             },
             imageOptions: {
                 hideBackgroundDots: true,
-                imageSize: 0.4,
+                imageSize: 0.2,
                 margin: 4
             },
+            //degradado interno
             dotsOptions: {
-                color: '#008D62',
                 type: 'dots',
-            },
-            backgroundOptions: {
-                color: '#FFFFFF'
-            },
-            dotsOptionsHelper: {
-                colorType: {
-                    single: true,
-                    gradient: false,
-                },
                 gradient: {
-                    linear: true,
-                    radial: false,
-                    color1: "#000000",
-                    color2: "#008D62",
-                    rotation: "0"
+                    type: "radial",
+                    rotation: 0,
+                    colorStops: [
+                        {
+                            offset: 0,
+                            color: "#000000"
+                        },
+                        {
+                            offset: 1,
+                            color: "#008D62"
+                        }
+                        
+                    ]
                 }
             },
+            //degradado de las esquinas
             cornersSquareOptions: {
-                type: "extra-rounded",
-                color: "#000000"
-            },
-            cornersSquareOptionsHelper: {
-                colorType: {
-                    single: true,
-                    gradient: false
-                },
                 gradient: {
-                    linear: true,
-                    radial: false,
-                    color1: "#000000",
-                    color2: "#000000",
-                    rotation: "0"
+                    type: "lineal",
+                    rotation: 0,
+                    colorStops: [
+                        
+                        {
+                            offset: 0,
+                            color: "#000000"
+                        },
+                        {
+                            offset: 1,
+                            color: "#008D62"
+                        },
+                    ]
                 }
             },
+            //degradado de los centros de la esquina
             cornersDotOptions: {
-                type: "square",
-                color: "#008D62"
-            },
-            cornersDotOptionsHelper: {
-                colorType: {
-                    single: true,
-                    gradient: false
-                },
+                color: "#000000",
                 gradient: {
-                    linear: true,
-                    radial: false,
-                    color1: "#000000",
-                    color2: "#000000",
-                    rotation: "0"
+                    type: "radial",
+                    rotation: 0,
+                    colorStops: [
+                        {
+                            offset: 0,
+                            color: "#000000"
+                        },
+                        {
+                            offset: 1,
+                            color: "#008D62"
+                        }
+                    ]
                 }
             },
-            backgroundOptionsHelper: {
-                colorType: {
-                    single: true,
-                    gradient: false
-                },
-                gradient: {
-                    linear: true,
-                    radial: false,
-                    color1: "#ffffff",
-                    color2: "#ffffff",
-                    rotation: "0"
-                }
-            }
         });
         qr.current.append(qrRef.current);
         qr.current.update({
