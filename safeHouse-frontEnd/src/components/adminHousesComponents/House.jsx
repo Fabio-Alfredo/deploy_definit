d82a899d452cc6d7aa778ec19990e6f1c4280437
AlexRobles00052121 <00052121@uca.edu.sv>
2024-05-13 22:34:27 -0600

    Create components for list of houses :wrench:

---
+++ safeHouse-frontEnd/src/components/adminHousesComponents/House.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+import { IoMdSettings } from "react-icons/io";
+import { FaStar } from "react-icons/fa";
+import UserList from "./userList";
+
+const House = ({ house }) => {
+    return (
+        <>
+            <div className='flex-col pb-3' >
+                <div className='flex justify-between pt-5'>
+                    <h2 className='flex font-popins sm:font-bold text-lg sm:text-2xl font-medium'>
+                        {house.direction}
+                    </h2>
+                    <div className='flex items-center   sm:pr-3'>
+                        <p className='flex font-xs font-popins font-xs '> Editar  </p>
+                        <IoMdSettings className='text-3xl pl-2 ' />
+                    </div>
+                </div>
+                <hr className='w-full bg-black mt-2 h-1' />
+            </div>
+            <div>
+                <div className='flex items-center'>
+                    <FaStar className='text-lg ' />
+                    <h3 className='flex font-popins text-lg pl-2'>  {house.owner.name}</h3>
+                </div>
+                <hr className='w-full bg-black h-0 sm:h-1 ' />
+            </div>
+            <UserList users={house.users} />
+        </>
+    );
+
+};
+
+export default House;
--- safeHouse-frontEnd/src/page/AdminHouse.jsx
+++ safeHouse-frontEnd/src/page/AdminHouse.jsx
@@ -1,9 +1,8 @@
 import Header from "../components/Header";
 import fetchHouses from "../service/fetchHouses";
-import { RiArrowLeftCircleLine } from "react-icons/ri";
-import { FaStar } from "react-icons/fa";
-import { IoMdSettings } from "react-icons/io";
 import { useEffect, useState } from "react";
+import NavBar from "../components/adminHousesComponents/navBar";
+import House from "../components/adminHousesComponents/House";
 
 const AdminHouse = () => {
     const [houses, setHouse] = useState([])
@@ -15,51 +14,22 @@ const AdminHouse = () => {
         };
 
         getHouse();
-    }, [])  
+    }, [])
 
     return (
         <div>
             <Header />
             <div className='bg-principalColor w-full flex-col h-[90vh] flex justify-center items-center p-6 '>
-                <div className='w-8/10 sm:w-1/2 bg-white h-3/4 shadow-lg br-20 p-8  rounded-2xl'>
-                    <nav className='flex felx-row items-center justify-center p-4'>
-                        <RiArrowLeftCircleLine className='hidden text-5xl  md:block' />
-                        <h2 className='font-brygada-1918 text-center flex-grow text-3xl sm:text-5xl'> Administracion de casa</h2>
-                    </nav>
+                <div className='w-8/10 sm:w-1/2 bg-white sm:h-3/4 h-[90%] shadow-lg br-20 p-8  rounded-2xl'>
 
-                    <div className="overflow-y-auto h-3/4">
+                    <NavBar />
+                    <div className="overflow-y-auto sm:h-3/4 h-[80%]">
                         {
                             houses.map((house) => (
-                                <div key={house.id}>
-                                    <div className='flex-col pb-3' >
-                                        <div className='flex justify-between pt-5'>
-                                            <h2 className='flex font-popins font-bold text-xl sm:text-2xl '>
-                                                {house.direction}
-                                            </h2>
-                                            <div className='flex items-center pr-3'>
-                                                <p className='flex font-xs font-popins font-xs '> Editar  </p>
-                                                <IoMdSettings className='text-3xl pl-2 ' />
-                                            </div>
-                                        </div>
-                                        <hr className='w-full bg-black mt-2 h-1' />
-                                    </div>
-                                    <div>
-                                        <div className='flex items-center'>
-                                            <FaStar className='text-lg ' />
-                                            <h3 className='flex font-popins text-lg pl-2'>  {house.owner.name}</h3>
-                                        </div>
-                                        <hr className='w-full bg-black  h-1' />
-                                    </div>
-                                    <ul>
-                                        {house.users.map((user) => (
-                                            <li className='font-popins pl-7 pt-2 text-lg' key={user.id}>
-                                                {user.name}
-                                                <hr className='w-full bg-black  h-1' />
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </div>
-                            ))}
+                                <House key={house.id} house={house} />
+                            ))
+                        }
+
                     </div>
                 </div>
             </div>
