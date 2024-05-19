21f1b41981e0e06b4489f0905c0e26261e67f9b9
AlexRobles00052121 <00052121@uca.edu.sv>
2024-05-18 20:49:31 -0600

    Add:

--- safeHouse-frontEnd/src/page/AdminHouse.jsx
+++ safeHouse-frontEnd/src/page/AdminHouse.jsx
@@ -23,7 +23,7 @@ const AdminHouse = () => {
                 <div className='w-8/10 lg:w-3/4 md:w-3/4 md:h-5/6 xl:w-1/2 bg-white sm:h-3/4 h-[90%] shadow-2xl br-20 p-8  rounded-3xl'>
 
                     <NavBar/>
-                    <div className="overflow-y-auto  sm:h-3/4 md:h-5/6 h-[80%]">
+                    <div className="overflow-y-auto  sm:h-3/4 md:h-5/6 h-5/6">
                         {
                             houses.map((house) => (
                                 <House key={house.id} house={house} />
