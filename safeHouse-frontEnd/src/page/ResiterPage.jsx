95f7bbe3f2862d5b59c498ef6c5b86dcaf5b79ae
Fabio-Alfredo <00210121@uca.edu.sv>
2024-05-22 23:44:13 -0600

    update: register page header

--- safeHouse-frontEnd/src/page/ResiterPage.jsx
+++ safeHouse-frontEnd/src/page/ResiterPage.jsx
@@ -3,6 +3,7 @@ import { Navigation, Input, CatalogRole } from '../components/registerComponents
 import Button from '../components/Button';
 import logo from '../assets/Logo.svg';
 import { useForm } from '../hooks/useForm';
+import Header from '../components/Header';
 
 
 const ResiterPage = () => {
@@ -26,9 +27,7 @@ const ResiterPage = () => {
 
     return (
         <div className='h-screen'>
-            <header className='bg-color-primary  flex justify-center sm:justify-start h-[10vh]'>
-                <img className='sm:p-2 pt-5 scale-75 ml-3 ' src={logo} />
-            </header>
+            <Header/>
             <div onSubmit={handleSubmit} className='flex items-center justify-center w-full bg-color-primary px-6 h-[90vh]'>
                 <form className=' w-full p-8 shadow-2xl rounded-3xl bg-white xl:h-3/4 xl:p-14 lg:w-1/2' > {/* query */}
                     <Navigation title={"Registrar usuario"} />
