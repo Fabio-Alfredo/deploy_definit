3bf753d5c2d632ab38986afba8003c84102e81f7
Fabio-Alfredo <00210121@uca.edu.sv>
2024-05-24 22:07:07 -0600

    add: style scanner

--- safeHouse-frontEnd/src/components/ReaderQrComponents/ContainerReaderQr.jsx
+++ safeHouse-frontEnd/src/components/ReaderQrComponents/ContainerReaderQr.jsx
@@ -4,7 +4,7 @@ import CreateReaderQr from './CreateReaderQr';
 
 const ContainerReaderQr = () => {
     return (
-        <div className='w-full p-8 shadow-2xl rounded-3xl bg-white h-fit lg:w-2/3  xl:w-1/2' > {/* query */}
+        <div className='w-full p-4 xl:p-10 shadow-2xl rounded-3xl bg-white h-fit lg:w-2/3  xl:w-1/2' > {/* query */}
             <Navigation title={"Escanear codigo QR"} />
             <CreateReaderQr/>
         </div>
--- safeHouse-frontEnd/src/components/ReaderQrComponents/CreateReaderQr.jsx
+++ safeHouse-frontEnd/src/components/ReaderQrComponents/CreateReaderQr.jsx
@@ -1,23 +1,43 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { QrReader } from 'react-qr-reader'
 
 const CreateReaderQr = () => {
     const [data, setData] = useState('No result');
     return (
-        <div>
+        <div className='flex py-4 justify-center items-center '>
             <QrReader
                 onResult={(result, error) => {
-                    if (!!result) {
+                    if (result) {
                         setData(result?.text);
                     }
 
-                    if (!!error) {
+                    if (error) {
                         console.info(error);
                     }
                 }}
-                style={{ width: '100%' }}
+                facingMode={'self'}
+                constraints={{ facingMode: 'environment' }}
+                scanDelay={5000}
+                containerStyle={{ 
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: '80%', 
+                    height: 'auto',
+                    margin: '0 auto',
+                    border: '2px solid #000',
+                    borderRadius: '10px',
+                    padding: '5px',
+                    borderColor: '#008D62',
+                }}
+                videoStyle={{
+                    flex: 1,
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'cover',
+                    borderRadius: '10px',
+                }}
             />
-            <p>{data}</p>
         </div>
     );
 };
