b3712b5c43befd95680b43804b719a52dcf1c9dd
Fabio-Alfredo <00210121@uca.edu.sv>
2024-06-01 11:13:52 -0600

    add: register entry
    update: responsive desing and personalization button

--- safeHouse-frontEnd/src/components/Input.jsx
+++ safeHouse-frontEnd/src/components/Input.jsx
@@ -34,7 +34,7 @@ const Input = ({ name, label, type, inputValue, inputOnchange }) => {
 
     return (
         <>
-            <div className='grid grid-cols-3 sm:mx-12 items-center m-4 sm:m-10'>
+            <div className='grid grid-cols-3 sm:mx-12 items-center m-2 lg:m-4 sm:m-10'>
                 <label htmlFor={name} className='col-span-1 font-popins text-sm sm:text-2xl'> {label} </label> {/* query */}
                 <input required type={type} name={name} value={inputValue} onChange={inputOnchange}
                     className='col-span-2 pl-3 ml-5 shadow-md font-popins bg-input-color rounded-md  text-xs xl:ml-0 sm:text-xl h-6 sm:h-10 w-10/12' />
--- safeHouse-frontEnd/src/page/RegisterEntry.jsx
+++ safeHouse-frontEnd/src/page/RegisterEntry.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Input from '../components/Input';
 import Navigation from '../components/Navigation';
+import Textarea from '../components/Textarea';
 import Button from '../components/Button';
 import Header from '../components/Header';
 import { useForm } from '../hooks/useForm';
@@ -28,12 +29,12 @@ const RegisterEntry = () => {
     return (
         <div className='h-screen'>
             <Header />
-            <div className='flex pt-48 sm:pt-0 sm:items-center justify-center w-full bg-color-primary px-6 min-h-[90vh]'>
-                <form onSubmit={handleSubmit} className=' w-full p-8 shadow-2xl rounded-3xl bg-white h-fit xl:pt-24 lg:w-2/3  xl:w-1/2' > {/* query */}
+            <div className='flex pt-32 sm:pt-0 sm:items-center justify-center w-full bg-color-primary px-6 min-h-[90vh]'>
+                <form onSubmit={handleSubmit} className='w-full p-8 lg:p-6 shadow-2xl rounded-3xl bg-white h-fit lg:w-2/3  xl:w-1/2' > {/* query */}
                     <Navigation title={"Registrar entrada"} />
                     <Input name={"company"} label={"Empresa:"} type={"text"} inputValue={company} inputOnchange={InputChange} />
                     <Input name={"name"} label={"Nombre:"} type={"text"} inputValue={name} inputOnchange={InputChange} />
-                    <Input name={"reason"} label={"Motivo:"} type={"textarea"} inputValue={reason} inputOnchange={InputChange} />
+                    <Textarea name={"reason"} rows={5} cols={20} label={"Motivo:"} inputValue={reason} inputOnchange={InputChange} />
                     <Button value={"Registrar"} type={"submit"} name={"RegisterEntry"} />
                 </form>
             </div>
