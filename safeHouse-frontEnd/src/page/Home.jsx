0d9a08d5afbf0edbc97fd88d85dca75b52578f0a
Nietoof6 <00087419@uca.edu.sv>
2024-05-31 23:56:55 -0600

    add: adding responsive design for profile card

--- safeHouse-frontEnd/src/page/Home.jsx
+++ safeHouse-frontEnd/src/page/Home.jsx
@@ -38,10 +38,10 @@ const Home = () => {
             </header>
             <div className="min-w-screen min-h-[90vh] flex flex-col justify-center items-center gap-10 bg-color-primary ">
                 <h1 className='w-full font-brygada-1918 text-2xl md:text-4xl xl:text-6xl xl:pl-36 text-center xl:text-start'>Bienvenido</h1>
-                <div className='w-[360px] h-[420px] md:w-[700px] md:h-[360px] p-8 md:p-12 bg-white grid grid-flow-dense grid-rows-5 grid-cols-1 md:grid-rows-3 md:grid-cols-3 justify-center items-center shadow-2xl rounded-xl'>
-                    <img className='w-44 h-44 rounded-full border-4 row-span-2 border-black' src={profile}/>
-                    <p className='md:col-span-2 text-4xl font-popins font-bold md:self-end'>Nombre Apellido</p>
-                    <p className='md:col-span-2 text-xl md:self-start'>administrador@gmail.com</p>
+                <div className='w-[360px] h-80 sm:h-[360px] md:w-[700px] md:h-[360px] p-8 md:p-12 bg-white grid grid-flow-dense grid-rows-5 grid-cols-1 md:grid-rows-3 md:grid-cols-3 justify-center items-center shadow-2xl rounded-xl'>
+                    <div className='row-span-2 flex justify-center'><img className='w-32 h-32 md:w-44 md:h-44 rounded-full border-4  border-black' src={profile}/></div>
+                    <p className='md:col-span-2 text-lg sm:text-xl md:text-4xl  font-popins font-bold text-center self-end'>Nombre Apellido</p>
+                    <p className='md:col-span-2 text-sm sm:text-base md:text-xl text-center self-start'>administrador@gmail.com</p>
                     <Button type={'button'} name={'Log out'} value={'Cerrar sesión'} />
                 </div>
                 <div className='flex gap-4 sm:gap-8 md:gap-10 xl:gap-12 flex-wrap justify-center items-center'>
