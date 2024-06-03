83c225659ff04d9b6a4da9fbb40b894789be06b0
Nietoof6 <00087419@uca.edu.sv>
2024-06-02 19:21:19 -0600

    fix: fixing responsive mobile design

--- safeHouse-frontEnd/src/page/Home.jsx
+++ safeHouse-frontEnd/src/page/Home.jsx
@@ -16,19 +16,20 @@ const Home = () => {
         {
             title: "Registrar entrada anonima",
             icon: FaUser,
-        },
-        {
-            title: "Administración de entradas",
-            icon: MdHouse,
-        },
-        {
-            title: "Administración de usuarios",
-            icon: MdPeopleAlt,
-        },
-        {
-            title: "Reporte de entradas",
-            icon: TbReportAnalytics,
         }
+        // ,
+        // {
+        //     title: "Administración de entradas",
+        //     icon: MdHouse,
+        // },
+        // {
+        //     title: "Administración de usuarios",
+        //     icon: MdPeopleAlt,
+        // },
+        // {
+        //     title: "Reporte de entradas",
+        //     icon: TbReportAnalytics,
+        // }
     ]
 
     return (
@@ -36,13 +37,13 @@ const Home = () => {
             <header className='bg-color-primary  flex justify-center sm:justify-start min-w-screen h-[10vh]'>
                 <img className='sm:p-2 pt-5 scale-75 ml-3 ' src={logo} />
             </header>
-            <div className="min-w-screen min-h-[90vh] flex flex-col justify-center items-center gap-10 bg-color-primary ">
+            <div className="min-w-screen min-h-[90vh] flex flex-col pt-6 md:pt-0 md:justify-center items-center gap-10 bg-color-primary ">
                 <h1 className='w-full font-brygada-1918 text-2xl md:text-4xl xl:text-6xl xl:pl-36 text-center xl:text-start'>Bienvenido</h1>
-                <div className='w-[360px] h-80 sm:h-[360px] md:w-[700px] md:h-[360px] p-8 md:p-12 bg-white grid grid-flow-dense grid-rows-5 grid-cols-1 md:grid-rows-3 md:grid-cols-3 justify-center items-center shadow-2xl rounded-xl'>
-                    <div className='row-span-2 flex justify-center'><img className='w-32 h-32 md:w-44 md:h-44 rounded-full border-4  border-black' src={profile}/></div>
+                <div className='w-[360px] h-80 sm:h-[360px] md:w-[700px] md:h-[360px] p-8 md:p-12 bg-white grid grid-flow-dense grid-rows-8 grid-cols-1 md:grid-rows-3 md:grid-cols-3 justify-center items-center shadow-2xl rounded-xl'>
+                    <div className='row-span-5 md:row-span-2 flex justify-center'><img className='w-32 h-32 md:w-44 md:h-44 rounded-full border-4  border-black' src={profile}/></div>
                     <p className='md:col-span-2 text-lg sm:text-xl md:text-4xl  font-popins font-bold text-center self-end'>Nombre Apellido</p>
                     <p className='md:col-span-2 text-sm sm:text-base md:text-xl text-center self-start'>administrador@gmail.com</p>
-                    <Button type={'button'} name={'Log out'} value={'Cerrar sesión'} />
+                    <Button type={'button'} name={'Log out'} value={'Cerrar sesión'}/>
                 </div>
                 <div className='flex gap-4 sm:gap-8 md:gap-10 xl:gap-12 flex-wrap justify-center items-center'>
                     {navigate.map((item) => (
