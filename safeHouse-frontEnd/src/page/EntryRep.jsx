9098dbdb85c964621dc914134fc691b3fce30717
Nietoof6 <00087419@uca.edu.sv>
2024-06-05 10:49:29 -0600

    add: adding entries report page

---
+++ safeHouse-frontEnd/src/page/EntryRep.jsx
@@ -0,0 +1,29 @@
+import { Component } from "react";
+import Header from "../components/Header";
+import TabView from "../components/TabView";
+
+const user = (<p>Hola</p>)
+
+const email = (<p>
+    que tal
+</p>)
+
+class EntryRep extends Component {
+
+    render() {
+        return (
+            <div className="min-w-screen min-h-screen">
+                <Header />
+                <div className='flex items-center justify-center w-full bg-color-primary px-6 min-h-[90vh]'>
+                    <TabView title={"Registros"}
+                        tabs={[
+                            { name: "Visitantes", content: user },
+                            { name: "Anonimo", content: email }
+                        ]} />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default EntryRep;
