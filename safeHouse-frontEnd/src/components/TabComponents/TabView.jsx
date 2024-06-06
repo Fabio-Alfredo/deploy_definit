93ec659b8f3471b3addede49fe9655a5f0bba74d
Nietoof6 <00087419@uca.edu.sv>
2024-06-05 22:47:12 -0600

    change: changing Tab view component

--- safeHouse-frontEnd/src/components/TabComponents/TabView.jsx
+++ safeHouse-frontEnd/src/components/TabComponents/TabView.jsx
@@ -1,39 +1,30 @@
-import { useState } from "react";
 import Navigation from "../Navigation";
+import EmptyReport from "../EmptyContent";
+import CardReport from "./CardReport";
 
-const TabView = ({ title, tabs = {} }) => {
-
-    const [activeTabIndex, setActiveTabIndex] = useState(0);
-
-    const activateTab = (index) => {
-        setActiveTabIndex(index);
-    }
+const TabView = ({ title, reports = {} }) => {
 
     return (
         <div className='w-full p-4 sm:p-8 shadow-2xl rounded-3xl bg-white h-fit lg:w-2/3  xl:w-1/2'>
             <Navigation title={title} />
-            <div className="body">
-                {Object.keys(tabs).length === 0 ?
-                    (<div> No tabs </div>)
+            <hr className='h-0.5 bg-black mb-8 mx-4' />
+            <div className="flex justify-center">
+                {Object.keys(reports).length === 0 ?
+                    (<EmptyReport message={'No hay entradas que reportar'} />)
                     :
                     (
-                    <div>
-                        <div className="tabs">
-                            {tabs.map((tab, index) => (
-                                <label
-                                key={index}
-                                className={index === activeTabIndex ? "active-tab" : "tab"}
-                                onClick={()=> activateTab(index)}>
-                                    {tab.name}
-                                </label>
+                        <div className="w-full max-h-fit">
+                            <div className='overflow-y-auto h-[35vh] md:h-[50vh] px-4'>
+                                {reports.map((tab, index) => (
+                                    <CardReport key={index} name={tab.name} dateVisit={tab.date} hourVisit={tab.hour} />
                                 )
-                            )}
+                                )}
+                            </div>
                         </div>
-                        <div> {tabs[activeTabIndex].content} </div>
-                    </div>
                     )
                 }
             </div>
+            <hr className='h-0.5 bg-black mb-8 mx-4' />
         </div>
     )
 }
