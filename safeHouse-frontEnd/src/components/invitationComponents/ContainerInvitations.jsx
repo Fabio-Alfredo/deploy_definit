33568ed38caa81ed9d4795f02b46f20f5a3df416
Fabio-Alfredo <00210121@uca.edu.sv>
2024-06-06 00:41:52 -0600

    add: message not invitations

--- safeHouse-frontEnd/src/components/invitationComponents/ContainerInvitations.jsx
+++ safeHouse-frontEnd/src/components/invitationComponents/ContainerInvitations.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Navigation from '../Navigation';
 import InvitationCard from '../invitationComponents/InvitationCard'
 import { fetchInvitation } from '../../service/fetchHouses';
+import EmptyReport from '../EmtyContent';
 
 
 const ContainerInvitations = () => {
@@ -11,31 +12,33 @@ const ContainerInvitations = () => {
 
         const getInvitations = async () => {
             const res = await fetchInvitation()
-            console.log({res});
+            console.log({ res });
             setInvitations(res)
         }
 
         getInvitations()
     }, [])
-    
+
     return (
         <>
             <div className='w-full p-4 sm:p-8 shadow-2xl rounded-3xl bg-white h-fit lg:w-2/3  xl:w-1/2' > {/* query */}
                 <Navigation title={"Invitaciones"} />
                 <hr className='h-0.5 bg-black mb-6 mx-4' />
-                <div className='overflow-y-auto h-[35vh] md:h-[50vh] px-4'>
-                    {
-                        invitations.length > 0 ?(
-                            invitations.map((_i) => (
-                                <InvitationCard key={_i.id} house={_i.house} fecha={_i.fecha} />
-                            ))
-                        ):(
-                            <div className='h-full flex flex-col items-center justify-center'>
-                                <p>vacio</p>
-                            </div>
-                        )
-                    }
-                </div>
+
+                {
+                    invitations.length > 0 ? (
+                        <div className='overflow-y-auto h-[35vh] md:h-[50vh] px-4'>
+                            {
+                                invitations.map((_i) => (
+                                    <InvitationCard key={_i.id} house={_i.house} fecha={_i.fecha} />
+                                ))
+                            }
+                        </div>
+
+                    ) : (
+                        <EmptyReport message="No tienes invitaciones pendientes" />
+                    )
+                }
                 <hr className='h-0.5 bg-black my-8 mx-4' />
             </div>
         </>
