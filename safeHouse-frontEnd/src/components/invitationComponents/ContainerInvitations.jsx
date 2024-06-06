7c8c807e79ac146c49eeacb055656bc9886efefb
Fabio-Alfredo <00210121@uca.edu.sv>
2024-06-05 21:54:02 -0600

    update: new style response and responsive design  vacio component

--- safeHouse-frontEnd/src/components/invitationComponents/ContainerInvitations.jsx
+++ safeHouse-frontEnd/src/components/invitationComponents/ContainerInvitations.jsx
@@ -24,11 +24,15 @@ const ContainerInvitations = () => {
             <div className='w-full p-4 sm:p-8 shadow-2xl rounded-3xl bg-white h-fit lg:w-2/3  xl:w-1/2' > {/* query */}
                 <Navigation title={"Invitaciones"} />
                 <hr className='h-0.5 bg-black mb-6 mx-4' />
-                <div className='overflow-y-auto max-h-[35vh] md:max-h-[50vh] px-4'>
+                <div className='overflow-y-auto h-[35vh] md:h-[50vh] px-4'>
                     {
-                        invitations.map((_i) => (
-                            <InvitationCard key={_i.id} house={_i.house} fecha={_i.fecha} />
-                        ))
+                        invitations.length > 0 ?(
+                            invitations.map((_i) => (
+                                <InvitationCard key={_i.id} house={_i.house} fecha={_i.fecha} />
+                            ))
+                        ):(
+                            <p>vacio</p>
+                        )
                     }
                 </div>
                 <hr className='h-0.5 bg-black my-8 mx-4' />
