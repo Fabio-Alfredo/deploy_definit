db9a5088ce6c05fc33ac488611918b06f2e58218
Fabio-Alfredo <00210121@uca.edu.sv>
2024-06-05 18:28:33 -0600

    update: design responsive

--- safeHouse-frontEnd/src/components/invitationComponents/InvitationCard.jsx
+++ safeHouse-frontEnd/src/components/invitationComponents/InvitationCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ButtonComponent from './ButtonComponent';
 
 const InvitationCard = () => {
     return (
         <>
 
-            <div className='font-popins flex items-center flex-wrap justify-between w-full px-3 mt-2 py-2 text-lg list-none rounded-xl hover:bg-slate-100 hover:-translate-y-1 duration-500 ' key={2}>
+            <div className='font-popins group/item flex items-center flex-wrap justify-between px-3 md:hover:px-3 md:px-6  w-full mt-2 py-2 text-lg list-none rounded-xl hover:bg-slate-100 hover:-translate-y-1 duration-500 ' key={2}>
                 <div className='flex flex-col justify-start items-start my-1'>
                     <p className='font-popins font-bold items-center p-0 md:px-2'>Invitacion</p>
                     <p className='font-popins text-gray-500 font-semibold items-center p-0 md:px-2'>Casa#2</p>
@@ -14,12 +15,12 @@ const InvitationCard = () => {
                     <p className='font-popins'>Fecha</p>
                     <p className='font-popins text-center  text-gray-500'>12/12/2021 </p>
                 </div>
-                <div className='flex items-center w-full md:w-fit justify-center md:justify-end gap-2 my-1'>
-                    <input type="button" value="aceptar" className='px-2 py-1 rounded-xl h-fit w-fit bg-green-300 cursor-pointer' />
-                    <input type="button" value="rechazar" className='px-2 py-1 rounded-xl h-fit w-fit bg-red-500 cursor-pointer' />
+                <div className=' items-center w-full hidden group-hover/item:flex md:w-fit justify-center md:justify-end gap-2 my-1'>
+                    <ButtonComponent value={"Rechazar"} className={'bg-red-600'}/>
+                    <ButtonComponent value={"Rechazar"} className={'bg-green-400'}/>
                 </div>
             </div>
-            <hr className='items-center bg-black px-5' />
+
 
         </>
     );
