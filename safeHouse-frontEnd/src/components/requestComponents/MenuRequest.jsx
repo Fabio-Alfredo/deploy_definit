2d5f35a6b1a1c1b4c946b2f9fb652a32a5ba0a15
Fabio-Alfredo <00210121@uca.edu.sv>
2024-06-08 22:43:44 -0600

    add: responsive design menu

--- safeHouse-frontEnd/src/components/requestComponents/MenuRequest.jsx
+++ safeHouse-frontEnd/src/components/requestComponents/MenuRequest.jsx
@@ -11,10 +11,10 @@ const MenuRequest = () => {
     return (
         <div>
 
-            <div onClick={handleEvent} className='w-full relative flex flex-col justify-center pr-8 pb-2 items-end cursor-pointer group/item'>
-                <ul className={`absolute right-11 -top-24 mt-2 p-1 rounded-xl bg-slate-100 shadow-lg ${state ? 'opacity-100 scale-100' : 'opacity-0 scale-95'} z-50`}>
-                    <li className='font-popins text-sm flex sm:text-lg lg:text-xl items-center select-none py-2 px-4 hover:bg-white duration-500 rounded-xl'>Casual</li>
-                    <li className='font-popins text-sm flex sm:text-lg lg:text-xl items-center select-none py-2 px-4 hover:bg-white  duration-500 rounded-xl'>Repetitiva</li>
+            <div onClick={handleEvent} className='w-full relative flex flex-col justify-center pr-4 sm:pr-8 pb-2 items-end cursor-pointer group/item'>
+                <ul className={`absolute right-7 sm:right-11  top-7 mt-2 p-1 rounded-lg bg-slate-100 shadow-lg ${state ? 'opacity-100 scale-100' : 'opacity-0 scale-95'} z-50`}>
+                    <li className='font-popins text-sm flex sm:text-lg lg:text-xl items-center select-none py-2 px-4 hover:bg-white duration-500 rounded-lg'>Casual</li>
+                    <li className='font-popins text-sm flex sm:text-lg lg:text-xl items-center select-none py-2 px-4 hover:bg-white  duration-500 rounded-lg'>Repetitiva</li>
                 </ul>
                 <p className='font-popins text-sm flex sm:text-lg lg:text-xl items-center select-none py-2 px-4 rounded-full hover:bg-slate-100 duration-500 '>
                     Nueva solicitud
