5e096ae0f5161ccda7da1ec3b527ca65551aa83f
Fabio-Alfredo <00210121@uca.edu.sv>
2024-06-08 21:11:25 -0600

    add: new solicitude

--- safeHouse-frontEnd/src/components/requestComponents/ContainerRequest.jsx
+++ safeHouse-frontEnd/src/components/requestComponents/ContainerRequest.jsx
@@ -1,7 +1,8 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import EmptyReport from '../EmtyContent';
 import Navigation from '../Navigation';
 import RequestCard from './RequestCard';
+import { FaCirclePlus } from "react-icons/fa6";
 import { fetchReques } from '../../service/fetchHouses';
 
 const ContainerRequest = () => {
@@ -22,7 +23,12 @@ const ContainerRequest = () => {
         <>
             <div className='w-full p-4 sm:p-8 shadow-2xl rounded-3xl bg-white h-fit lg:w-2/3  xl:w-1/2' > {/* query */}
                 <Navigation title={"Solicitudes"} />
+
                 <hr className='h-0.5 bg-black mb-6 mx-4' />
+                <div className='w-full flex  justify-end pr-8 items-center cursor-pointer group/item'>
+                    <p className='font-popins text-sm sm:text-lg lg:text-xl items-center px-2 '>Nueva solicitud</p>
+                    <FaCirclePlus className='text-lg sm:text-2xl lg:text-3xl group-hover/item:scale-110 duration-300' />
+                </div>
                 {
                     request.length > 0 ? (
                         <div className='overflow-y-auto h-[35vh] md:h-[50vh] px-4'>
