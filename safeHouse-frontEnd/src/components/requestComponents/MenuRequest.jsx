96163a4e5438798e0e44b4cd3d8a9b9937d5ecf0
Fabio-Alfredo <00210121@uca.edu.sv>
2024-06-09 16:10:46 -0600

    update: responsive desig

--- safeHouse-frontEnd/src/components/requestComponents/MenuRequest.jsx
+++ safeHouse-frontEnd/src/components/requestComponents/MenuRequest.jsx
@@ -2,17 +2,19 @@ import React, { useState } from 'react';
 import { FaCirclePlus } from "react-icons/fa6";
 
 const MenuRequest = () => {
+
     const [state, setstate] =
         useState(false)
 
     const handleEvent = () => {
         setstate(!state)
     }
-    return (
-        <div>
 
-            <div onClick={handleEvent} className='w-full relative flex flex-col justify-center pr-4 sm:pr-8 pb-2 items-end cursor-pointer group/item'>
-                <ul className={`absolute right-7 sm:right-11  top-7 mt-2 p-1 rounded-lg bg-slate-100 shadow-lg ${state ? 'opacity-100 scale-100' : 'opacity-0 scale-95'} z-50`}>
+    return (
+        <div className='w-full flex justify-end items-center '>
+
+            <div onClick={handleEvent} className='relative pr-4 sm:pr-8 pb-2 cursor-pointer group/item'>
+                <ul className={`absolute right-7 sm:right-11  top-7 mt-2 p-1 rounded-lg bg-slate-100 shadow-md ${state ? 'opacity-100 scale-100' : 'opacity-0 scale-95'} z-50`}>
                     <li className='font-popins text-sm flex sm:text-lg lg:text-xl items-center select-none py-2 px-4 hover:bg-white duration-500 rounded-lg'>Casual</li>
                     <li className='font-popins text-sm flex sm:text-lg lg:text-xl items-center select-none py-2 px-4 hover:bg-white  duration-500 rounded-lg'>Repetitiva</li>
                 </ul>
