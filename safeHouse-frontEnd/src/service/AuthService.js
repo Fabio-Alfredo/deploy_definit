75fd8d500c17136727355ba4c565adeeadee6a79
Fabio-Alfredo <00210121@uca.edu.sv>
2024-06-24 17:22:30 -0600

    add: login connection

--- safeHouse-frontEnd/src/service/AuthService.js
+++ safeHouse-frontEnd/src/service/AuthService.js
@@ -1,20 +1,37 @@
 import axios from "axios";
+import { json } from "react-router-dom";
 
-const BASE_URL = 'http://localhost:8080/api/';
+const BASE_URL = 'http://localhost:8080/api';
+
+const getToken = ()=> JSON.parse(localStorage.getItem('token')).token;
 
 export const AuthConecction = async (data) => {
 
     try {
-        const response = await axios.get(`${BASE_URL}auth/login`, data,{
+        const res = await axios.post(`${BASE_URL}/auth/login`, data,{
             headers: { 
                 'Content-Type': 'application/json',
              }
         });
-        return response;
+        return res.data;
     } catch (error) {
 
         console.log(error);
-        throw new Error('Error al obtener los datos');
+        throw new Error(error);
     }
 
+}
+
+export const GetUserData = async () => {
+    try{
+        const res = await axios.get(`${BASE_URL}/user/one`,{
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${getToken()}`
+            }
+        });
+        return res.data;
+    }catch(error){
+        console.log(error);
+    }
 }
