7715faaab05956a3094495881d0af504ef3d5938
Fabio-Alfredo <00210121@uca.edu.sv>
2024-06-29 14:16:20 -0600

    update: connectin by api

--- safeHouse-frontEnd/src/components/graphicsComponents/LinearChartGraphic.jsx
+++ safeHouse-frontEnd/src/components/graphicsComponents/LinearChartGraphic.jsx
@@ -2,22 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import { GetEntrysByMonth } from '../../service/RequestService';
 
-const entradasPorMes = [
-    { mes: "Ene", entradas: 1200 },
-    { mes: "Feb", entradas: 1100 },
-    { mes: "Mar", entradas: 1300 },
-    { mes: "Abr", entradas: 1400 },
-    { mes: "May", entradas: 1350 },
-    { mes: "Jun", entradas: 1450 },
-    { mes: "Jul", entradas: 1500 },
-    { mes: "Ago", entradas: 1550 },
-    { mes: "Sep", entradas: 1400 },
-    { mes: "Oct", entradas: 1300 },
-    { mes: "Nov", entradas: 1200 },
-    { mes: "Dic", entradas: 1600 }
-
-];
-
 const abbreviations = {
     "JANUARY": "Ene",
     "FEBRUARY": "Feb",
