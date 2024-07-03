d6c373b5c5380f4a0160731bb499b2baa87438cd
Fabio-Alfredo <00210121@uca.edu.sv>
2024-07-03 12:19:10 -0600

    update: update resident admin

--- safeHouse-frontEnd/src/components/adminHousesComponents/House.jsx
+++ safeHouse-frontEnd/src/components/adminHousesComponents/House.jsx
@@ -6,6 +6,7 @@ import { AiTwotoneDelete } from "react-icons/ai";
 import Swal from "sweetalert2";
 import { useState } from "react";
 import { AssignAdminHouse } from "../../service/UserService";
+import AdminHouseComponent from "./AdminHouseComponent";
 
 
 
@@ -51,6 +52,8 @@ const House = ({ house, state, updateState }) => {
         }
     }
 
+
+
     return (
         <div>
             <div className='flex-col pb-1' >
@@ -66,19 +69,7 @@ const House = ({ house, state, updateState }) => {
                 <hr className='w-full bg-black mt-2 h-1' />
             </div>
             <div>
-                <div className='flex items-center'>
-                    <FaStar className={`text-lg ${state ? '' : 'hidden'} `} />
-                    <h3 className='flex font-popins w-4/5  text-lg pl-2'>
-                        {/* {house.owner.name} */}
-                        {
-                            house.residentAdmin ? house.residentAdmin.name : 'No hay residentes'
-                        }
-                    </h3>
-                    <div className={`w-full flex items-center justify-end pr-2 group cursor-pointer ${state ? '' : 'hidden'}`}>
-                        <p className='font-xs font-popins font-xs group-hover:block hidden text-red-500'> Eliminar  </p>
-                        <AiTwotoneDelete className='text-3xl pl-1 text-end group-hover:text-red-500' />
-                    </div>
-                </div>
+                <AdminHouseComponent state={state} house={house} />
                 <hr className='w-full bg-black h-0 sm:h-1 ' />
             </div>
             <UserList users={house.users} house={house.id} state={state} />
--- safeHouse-frontEnd/src/service/HouseService.js
+++ safeHouse-frontEnd/src/service/HouseService.js
@@ -1,46 +1,61 @@
 import axios from "axios";
 
 const BASE_URL = 'http://localhost:8080/api/house/';
-const getToken = ()=> JSON.parse(localStorage.getItem('token')).token;
+const getToken = () => JSON.parse(localStorage.getItem('token')).token;
 
 export const GetHouseData = async () => {
-    try{
-        const res = await axios.get(`${BASE_URL}all?filter=${''}`,{
+    try {
+        const res = await axios.get(`${BASE_URL}all?filter=${''}`, {
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${getToken()}`
             }
         });
         return res.data;
-    }catch(error){
+    } catch (error) {
         throw error.response;
     }
 }
 
 export const GetHouseEmty = async (filter) => {
-    try{
-        const res = await axios.get(`${BASE_URL}all?filter=${filter}`,{
+    try {
+        const res = await axios.get(`${BASE_URL}all?filter=${filter}`, {
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${getToken()}`
             }
         })
         return res.data;
-    }catch(error){
+    } catch (error) {
         throw error.response;
     }
 }
 
 export const DeleteResident = async (house, user) => {
-    try{
-        const res = await axios.delete(`${BASE_URL}delete?houseId=${house}&email=${user}`,{
+    try {
+        const res = await axios.delete(`${BASE_URL}delete?houseId=${house}&email=${user}`, {
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${getToken()}`
             }
         })
         return res.data;
-    }catch(error){
+    } catch (error) {
+        throw error.response;
+    }
+}
+
+export const UpdateResidentAdmin = async (data) => {
+    console.log(data);
+    try {
+        const res = await axios.patch(`${BASE_URL}assign/new-admin`, data, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${getToken()}`
+            }
+        })
+        return res.data;
+    } catch (error) {
         throw error.response;
     }
 }
