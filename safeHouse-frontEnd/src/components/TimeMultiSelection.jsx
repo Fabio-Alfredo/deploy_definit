ca64e8924cce6356bf48ea990438300533ea21e2
Nietoof6 <00087419@uca.edu.sv>
2024-07-03 19:48:49 -0600

    fix: fixing multiSelection structure

--- safeHouse-frontEnd/src/components/TimeMultiSelection.jsx
+++ safeHouse-frontEnd/src/components/TimeMultiSelection.jsx
@@ -3,13 +3,13 @@ import DatePicker, { DateObject, Calendar } from 'react-multi-date-picker';
 import TimePicker from 'react-multi-date-picker/plugins/time_picker';
 import Datepanel from 'react-multi-date-picker/plugins/date_panel'
 
-const TimeMultiSelection = ({ name, label, inputValue,inputOnchange, inputValue1, inputOnchange1 }) => {
-    
-        return (
-            <>
-                <div className='grid grid-cols-3 sm:mx-12 items-center m-4 lg:m-4 xl:m-8 '>
-                    <label htmlFor={name} className='col-span-1 font-popins text-sm sm:text-2xl'> {label} </label> {/* query */}
-                    <div className='col-span-1 flex overflow-hidden flex-col sm:ml-11 shadow-md font-popins bg-input-color rounded-md h-10 '>
+const TimeMultiSelection = ({ name, label, inputValue, inputOnchange, inputValue1, inputOnchange1 }) => {
+
+    return (
+        <>
+            <div className='grid grid-cols-3 sm:mx-12 items-center m-4 lg:m-4 xl:m-8 '>
+                <label htmlFor={name} className='col-span-1 font-popins text-sm sm:text-2xl'> {label} </label> {/* query */}
+                <div className='col-span-1 flex overflow-hidden flex-col sm:ml-11 shadow-md font-popins bg-input-color rounded-md h-10 '>
                     <DatePicker
                         disableDayPicker
                         format='HH:mm'
@@ -49,9 +49,9 @@ const TimeMultiSelection = ({ name, label, inputValue,inputOnchange, inputValue1
                         ]}
                     />
                 </div>
-                </div>
-            </>
-        );
+            </div>
+        </>
+    );
 }
 
 export default TimeMultiSelection;
