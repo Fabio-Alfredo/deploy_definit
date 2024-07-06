f94fa45377cece2c64ae1f7dde72cbb3bf003d0d
Fabio-Alfredo <00210121@uca.edu.sv>
2024-07-06 16:31:54 -0600

    add: Environment Variables

--- safeHouse-frontEnd/src/service/RequestService.js
+++ safeHouse-frontEnd/src/service/RequestService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:8080/api/request';
+const BASE_URL = import.meta.env.VITE_REQUEST_SERVICE;
 
 import { decryptData } from "../utils/encrypt";
 
