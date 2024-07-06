1486357b4540bd1873a93f4ae50d96c58295a057
Nietoof6 <00087419@uca.edu.sv>
2024-07-06 02:03:27 -0600

    add: adding connection to service add resident

--- safeHouse-frontEnd/src/components/adminHousesComponents/House.jsx
+++ safeHouse-frontEnd/src/components/adminHousesComponents/House.jsx
@@ -4,29 +4,17 @@ import UserList from "./userList";
 import { FaHouseUser } from "react-icons/fa";
 import { AiTwotoneDelete } from "react-icons/ai";
 import Swal from "sweetalert2";
-import { useState } from "react";
-import { AssignAdminHouse } from "../../service/UserService";
+import { useEffect, useState } from "react";
+import { AssignAdminHouse, AssignUsersHouse } from "../../service/UserService";
 import AdminHouseComponent from "./AdminHouseComponent";
 import { IoSettingsOutline } from "react-icons/io5";
 
 
 
-const House = ({ house, state, updateState }) => {
+const House = ({ house, state, updateState, getList }) => {
 
     const handleUpdateHouse = async (houseAddres) => {
-        // const { value: email } = await Swal.fire({
-        //     title: "Asignar nuevo administrador",
-        //     input: "email",
-        //     inputLabel: "Ingrese el correo del nuevo administrador",
-        //     showCancelButton: true,
-        //     confirmButtonText: "Asignar",
-        //     confirmButtonColor: "#008D62",
-        //     inputPlaceholder: "example@gmail.com"
-        // });
-        // if (email) {
-        //     handleConnect(houseAddres, email);
-        //     console.log(houseAddres, email);
-        // }
+
         const { value: formValues } = await Swal.fire({
             title: "Asignar nuevo administrador",
             html: `
@@ -64,7 +52,7 @@ const House = ({ house, state, updateState }) => {
 
 
     const handleConnect = async (houseAddres, email) => {
-        
+
         try {
             const res = await AssignAdminHouse({ house: houseAddres, email: [email] });
             Swal.fire({
@@ -93,6 +81,71 @@ const House = ({ house, state, updateState }) => {
     }
 
 
+    const handleAddResident = async (houseAddres) => {
+
+        const { value: formValues } = await Swal.fire({
+            title: "Asignar  nuevo residente",
+            html: `
+                <div class="flex flex-col">
+                    <label for="swal-input2">Nuevo residente:</label>
+                    <input id="swal-input2" type="email" class="swal2-input peer disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                        invalid:border-red-500 invalid:text-red-600
+                        focus:invalid:border-red-500 focus:invalid:ring-red-500 rounded-lg">
+                        <p class="invisible peer-invalid:visible text-red-500 text-sm">Ingrese un email valido.</p>
+                    </label>
+                </div>
+              `,
+            confirmButtonColor: "#008D62",
+            focusConfirm: false,
+            preConfirm: () => {
+                const email1 = document.getElementById("swal-input2").value
+
+                if (!email1) {
+                    Swal.showValidationMessage("Todos los campos son requeridos");
+                    return false;
+                }
+                const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                if (!emailPattern.test(email1)) {
+                    Swal.showValidationMessage("Por favor, introduce correos electrónicos válidos");
+                    return false;
+                }
+                return { email1: email1 }
+            }
+        });
+        if (formValues) {
+            handleConnectResident(houseAddres, formValues.email1);
+        }
+    }
+
+    const handleConnectResident = async (houseAddres, email) => {
+
+        try {
+            const res = await AssignUsersHouse({ house: houseAddres, emails: [email] });
+            Swal.fire({
+                position: "center",
+                icon: "success",
+                title: `${res.message}`,
+                confirmButtonColor: "#008D62",
+                showConfirmButton: false,
+                timer: 1500
+            });
+        
+
+        } catch (e) {
+            await Swal.fire({
+                position: "center",
+                icon: "error",
+                confirmButtonColor: "#008D62",
+                title: `${e.data.message}`,
+                showConfirmButton: false,
+                timer: 1500
+            });
+
+        }
+    }
+
+
+
 
     return (
         <div>
@@ -102,7 +155,7 @@ const House = ({ house, state, updateState }) => {
                         {/* {house.direction} */}Casa # {house.address}
                     </h2>
 
-                    <div onClick={() => handleUpdateAdmin(house)} className={` flex items-center justify-end pr-2 group cursor-pointer transition ease-in-out hover:-translate-y-1 hover:scale-100  ${state ? '' : 'hidden'}`}>
+                    <div onClick={() => handleAddResident(house.address)} className={` flex items-center justify-end pr-2 group cursor-pointer transition ease-in-out hover:-translate-y-1 hover:scale-100  ${state ? '' : 'hidden'}`}>
                         <p className='font-xs font-popins font-xs text-black-500'> Actualizar  </p>
                         < IoSettingsOutline className='text-3xl pl-1 text-end group-hover:text-black-500' />
                     </div>
